fba204d6506625cfe6a608e92b872665f3679a39
nanguage <nanguage@yahoo.com>
2024-02-02 00:20:55 +0100

    fix build bug

--- chain-thought/vite.config.ts
+++ chain-thought/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/chain-thought/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['stream']
+    })
+  ],
+  resolve: {
+    alias: {
+      'stream': 'stream-browserify',
+    },
+  },
 })
